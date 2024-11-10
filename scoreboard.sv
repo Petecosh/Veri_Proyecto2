34421f7ce515b27051678bb9216ab70d21acc36f
Josue096 <josu3qb@gmail.com>
2024-11-09 20:39:40 -0600

    xp

--- scoreboard.sv
+++ scoreboard.sv
@@ -101,8 +101,7 @@ class scoreboard extends uvm_scoreboard;
             exp_sc = exp_sc + 1;
             end
             frc_Z_norm[26:3] = {0, frc_Z_norm[24:1]};
-          else frc_Z_norm[26:3] = {0, frc_Z_norm[25:2]};
-        end
+          end else frc_Z_norm[26:3] = {0, frc_Z_norm[25:2]};
       end
 
       default: begin
