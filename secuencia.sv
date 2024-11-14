e11e250c6f4c988072b6cd883929871ea6f208c7
Josue096 <josu3qb@gmail.com>
2024-11-13 22:21:19 -0600

    delays

--- secuencia.sv
+++ secuencia.sv
@@ -13,20 +13,23 @@ class gen_secuencia extends uvm_sequence;
   constraint const_delay {1 < delay < 10;}
 
   virtual task body();
+    // Generamos la cantidad de items especificada en `cantidad_item`
     for (int i = 0; i < cantidad_item; i++) begin
-      delay.randomize();
-      $display("delay = %d",delay);
-      while (delay > 0) begin
-        delay = delay - 1;
-        #10;
-      end
-      begin
+      // Generamos un valor aleatorio para `delay`
+      assert(delay.randomize()) else `uvm_error("SEQ", "Fallo al randomizar delay");
+
+      // Imprimimos el delay generado
+      $display("delay = %d", delay);
+
+      // Introducimos el delay
+      #(delay * 10);
+
+      // Creamos y procesamos el item
       item_seq item = item_seq::type_id::create("item");
       start_item(item);
-      item.randomize();
+      assert(item.randomize()) else `uvm_error("SEQ", "Fallo al randomizar item");
       `uvm_info("SEQ", $sformatf("Generado nuevo item: %s", item.print()), UVM_HIGH);
       finish_item(item);
-      end
     end
     `uvm_info("SEQ", $sformatf("Completada generacion de %0d items", cantidad_item), UVM_LOW);
   endtask
