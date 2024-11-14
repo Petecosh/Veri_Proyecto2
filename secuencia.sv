4544359cbb76b32fad58fc375f727915888ad63c
Josue096 <josu3qb@gmail.com>
2024-11-13 22:09:55 -0600

    delays

--- secuencia.sv
+++ secuencia.sv
@@ -7,11 +7,19 @@ class gen_secuencia extends uvm_sequence;
   endfunction
 
   rand int cantidad_item;
+  rand int delay;
 
   constraint const_cantidad {250 < cantidad_item < 500;}
+  constraint const_delay {1 < delay < 10;}
 
   virtual task body();
     for (int i = 0; i < cantidad_item; i++) begin
+      delay.randomize();
+      $display("delay = %d",delay);
+      while (delay > 0) begin
+        delay = delay - 1;
+        #10
+      end 
       item_seq item = item_seq::type_id::create("item");
       start_item(item);
       item.randomize();
