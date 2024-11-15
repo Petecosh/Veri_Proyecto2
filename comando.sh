9fd97e9b26c8428366766fc916c48d3d31d90021
Petecosh <prodrguez4@gmail.com>
2024-11-15 13:10:16 -0600

    Comando sh con cobertura

--- comando.sh
+++ comando.sh
@@ -3,7 +3,7 @@ rm -rfv `ls |grep -v ".*\.sv\|.*\.sh"`;
 
 vcs -Mupdate test_bench.sv  -o salida -full64 -debug_all -sverilog -l log_test -ntb_opts uvm-1.2 +lint=TFIPC-L -cm line+tgl;
 
-./salida +UVM_VERBOSITY=UVM_HIGH +UVM_TESTNAME=base_test +UVM_TIMEOUT=20000 +ntb_random_seed=1 > deleteme_log_1
+./salida +UVM_VERBOSITY=UVM_HIGH +UVM_TESTNAME=base_test +UVM_TIMEOUT=20000 +ntb_random_seed=1 -cm line+tgl > deleteme_log_1
 
 #./salida -cm line+tgl+cond+fsm+branch+assert;
 #dve -full64 -covdir salida.vdb &
